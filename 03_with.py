b6a2c869d6658c9d3f9b8ec17af9ed19bafb1781
lixiang30 <534551900@qq.com>
2019-01-01 22:05:46 +0800

    with的用法

---
+++ 03_with.py
@@ -0,0 +1,34 @@
+
+####传统方法################
+#创建一个文件对象
+f = open("./1.text","w")
+# 向文件写内容
+f.write("hello flask")
+#关闭文件
+f.close()
+
+#########with方法#############
+
+with open('./2.text',"w") as f:
+    f.write("hello django,hello flask,hello tornado")
+
+
+"""
+with称为上下文管理器，
+"""
+
+class Foo():
+    def __enter__(self):
+        """进入with语句时被with调用"""
+        print("enter")
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """离开with语句时被with调用"""
+        print("exit")
+        print("exc_type:%s"%exc_type)
+        print("exc_val:%s"%exc_val)
+        print("exc_tb:%s"%exc_tb)
+
+with Foo() as foo:
+    print("hello python")
+    a = 1 / 0
+    print("111")
